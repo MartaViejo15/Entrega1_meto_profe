f308c9fc9fa7977e70af411ee5a8bece776fa60a
rober <robertrafael.rrsm@gmail.com>
2025-02-26 22:28:22 +0100

    Interface Movable

---
+++ src/es/uah/matcomp/mp/e1/d/abstraccionInterfaces/InterfaceMov/Movable.java
@@ -0,0 +1,9 @@
+package es.uah.matcomp.mp.e1.d.abstraccionInterfaces.InterfaceMov;
+
+public interface Movable {
+
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
---
+++ src/es/uah/matcomp/mp/e1/d/abstraccionInterfaces/InterfaceMov/MovableCircle.java
@@ -0,0 +1,33 @@
+package es.uah.matcomp.mp.e1.d.abstraccionInterfaces.InterfaceMov;
+
+public class MovableCircle implements Movable {
+    private int radius;
+    private MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+
+    public String toString(){
+        return "(" + center.x + "," + center.y + "), speed=(" + center.xSpeed + "," + center.ySpeed + "), radius= " + radius;
+    }
+
+    @Override
+    public void moveUp(){
+        center.moveUp();
+    }
+    @Override
+    public void moveDown(){
+        center.moveDown();
+    }
+
+    @Override
+    public void moveLeft(){
+        center.moveLeft();
+    }
+    @Override
+    public void moveRight(){
+        center.moveRight();
+    }
+}
